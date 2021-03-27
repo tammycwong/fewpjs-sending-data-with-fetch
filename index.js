e39f834598282df7df269bea66d12b952441b312
Tammy <tammycwong@gmail.com>
2021-03-27 11:45:02 -0400

    finished

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData(name, email) {
+      const configurationObject = {
+        method: "POST",
+            headers: {
+                "Content-Type" : "application/json",
+                "Accept" : "application/json"
+            },
+            body: JSON.stringify({
+                name: `${name}`,
+                email: `${email}`
+            })
+       };
+       return fetch("http://localhost:3000/users", configurationObject)
+        .then(function(response){
+          return response.json();
+    })
+        .then(function(response) {
+          document.body.innerHTML = response["id"];
+    })
+        .catch(function(error) {
+          document.body.innerHTML = error.message;
+    });
+}
